4d88b388a6e901dbcd88f8ae494cf56bfc709f23
Jessica Fix and Taylor Pokoj <jcfix.pdx@gmail.com>
2016-03-16 09:57:13 -0700

    Add functionality to api for dino ipsum

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,11 +1,25 @@
-// var Constructor = require('../js/function-name.js').Constructor;
-//
-// $(document).ready(function() {
-//
-// var newConstructor = new Constructor();
-//
-//
-// });
+// var DinoText = require('../js/function-name.js').DinoText;
+// var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $("form.generateIpsum").submit(function(event) {
+    debugger;
+    event.preventDefault();
+
+    var inputtedParagraphs = $("input#numParagraphs").val();
+    var inputtedNumber = $("input#numWords").val();
+
+    $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=' + inputtedParagraphs + '&words=' + inputtedNumber).then(function(response) {
+      $(".result").append(response);
+
+    });
+  });
+});
 
 
-/* Front-end User interface (jquery) code goes in this file */
+// /* Front-end User interface (jquery) code goes in this file */
+
+
+
+
+// var newDinoText = new DinoText(inputtedParagraphs, inputtedWords);
--- js/function-name.js
+++ js/function-name.js
@@ -1,10 +1,16 @@
-// exports.Constructor = function(argument){
-//   this.argument = argument;
+// exports.DinoText = function(numParagraphs, numWords){
+//   this.numParagraphs = numParagraphs;
+//   this.numWords = numWords;
 // };
 //
-// exports.Constructor.prototype.method = function() {
-//
+// exports.DinoText.prototype.paragraphGenerator = function() {
+//   return
 // };
+//
+// exports.DinoText.prototype.wordGenerator = function() {
+//   this.numWords
+// };
+
 
 
 /* functional backend code goes in this file */
